c7dbe3dba6f61433dd3db48f7c4c333f7e00431d
PhoenixKnight <mefisto40000x@gmail.com>
2019-08-19 01:30:48 +0300

    Добавлено главное меню

---
+++ My2DPlatformerForGeekBrains/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
